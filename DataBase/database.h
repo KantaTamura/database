7349bb1927807f4be65c39e5f06afa0606881dfe
PhiZero <kanta03085@outlook.jp>
2020-09-30 22:39:14 +0900

    mini person struct

---
+++ DataBase/database.h
@@ -0,0 +1,14 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+typedef struct {
+
+} Data;
+
+typedef struct {
+    char name[32];
+    char id[32];
+    char className[32];
+} Person;
+
+#endif //DATABASE_H
