2eadaaa280698824cd9bd863b496840c7b71dc40
PhiZero <kanta03085@outlook.jp>
2020-10-11 13:19:59 +0900

    CLI parse lists

---
+++ CLI/args.h
@@ -0,0 +1,22 @@
+#ifndef ARGS_H
+#define ARGS_H
+
+typedef enum Action {
+    Show,
+    File,
+    Add,
+} Action;
+
+typedef enum Args {
+    Name,
+    sNumber,
+    aNumber,
+    Address,
+    Gender,
+    Class,
+    Age,
+    Birth,
+    Score,
+} Args;
+
+#endif //ARGS_H
--- CLI/parse_args.h
+++ CLI/parse_args.h
@@ -1,8 +1,15 @@
 #ifndef PARSE_ARGS_H
 #define PARSE_ARGS_H
 
+#include "args.h"
+#include "../string/sds.h"
+
 typedef struct Cli {
-    
+    Action action;
+    Args args;
+    sds path;
+    int data_i32;
+    sds data_sds;
 } Cli;
 
 #endif //PARSE_ARGS_H
