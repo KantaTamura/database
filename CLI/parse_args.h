d985530cf18d91049c300a767406e6ee403bf8ae
PhiZero <kanta03085@outlook.jp>
2020-10-11 13:01:46 +0900

    Dynamic String Library test

---
+++ CLI/parse_args.h
@@ -0,0 +1,8 @@
+#ifndef PARSE_ARGS_H
+#define PARSE_ARGS_H
+
+typedef struct Cli {
+    
+} Cli;
+
+#endif //PARSE_ARGS_H
--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include "string/sds.h"
 
-int main() {
-    printf("Hello, World!\n");
+int main(int args, char* argv[]) {
+    sds string = sdsnew("Hello World!");
+    printf("%s\n", string);
+    sdsfree(string);
     return 0;
 }
